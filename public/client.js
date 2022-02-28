f6c0c93b555f2d1c93a643823b0687b46225d696
Tu-Code <solarinsarah3@gmail.com>
2022-02-28 22:52:29 +0100

    Formatted doc

--- public/client.js
+++ public/client.js
@@ -5,28 +5,28 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 const manager = new THREE.LoadingManager();
-manager.onStart = function ( url, itemsLoaded, itemsTotal ) {
+manager.onStart = function (url, itemsLoaded, itemsTotal) {
 
-	console.log( 'Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.' );
-
-};
-
-manager.onLoad = function ( ) {
-
-	console.log( 'Loading complete!');
+    console.log('Started loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.');
 
 };
 
+manager.onLoad = function () {
 
-manager.onProgress = function ( url, itemsLoaded, itemsTotal ) {
-
-	console.log( 'Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.' );
+    console.log('Loading complete!');
 
 };
 
-manager.onError = function ( url ) {
 
-	console.log( 'There was an error loading ' + url );
+manager.onProgress = function (url, itemsLoaded, itemsTotal) {
+
+    console.log('Loading file: ' + url + '.\nLoaded ' + itemsLoaded + ' of ' + itemsTotal + ' files.');
+
+};
+
+manager.onError = function (url) {
+
+    console.log('There was an error loading ' + url);
 
 };
 
@@ -34,21 +34,21 @@ const loader = new GLTFLoader(manager);
 
 let object;
 //show input 
-document.getElementById("button").onclick = function(){changeFilter()};
-function changeFilter(){
+document.getElementById("button").onclick = function () { changeFilter() };
+function changeFilter() {
     document.getElementById("imageFile").style.display = 'block';
 }
 //remove previous object
-document.getElementById("check").onclick = function(){checkFile()};
+document.getElementById("check").onclick = function () { checkFile() };
 function removeEntity(object) {
-    if(scene.remove(object ))
+    if (scene.remove(object))
         alert("Just removed" + object)
     else
         alert("error")
     animate();
 }
 
-function checkFile(result){
+function checkFile(result) {
     //Get the file input element by its id 
     var fileInput = document.getElementById('imageFile');
     //Get the file name
@@ -58,52 +58,52 @@ function checkFile(result){
     //Get the file Extension 
     var fileExtension = (fileName).match(patternFileExtension);
     // alert(fileExtension);
-    if(fileExtension == '.gltf,gtf' || fileExtension == '.glb,glb'){
+    if (fileExtension == '.gltf,gtf' || fileExtension == '.glb,glb') {
         //remove old object
         removeEntity(object)
         loader.load(result,
-        function(gltf){
-            object = gltf.scene;
-            scene.add(gltf.scene);
-            alert("Uploaded " + fileName + " succesfully.")
-        }, undefined, function ( error ) {
+            function (gltf) {
+                object = gltf.scene;
+                scene.add(gltf.scene);
+                alert("Uploaded " + fileName + " succesfully.")
+            }, undefined, function (error) {
 
-            console.error( error );
-        } );
-    }  
+                console.error(error);
+            });
+    }
 
 
-    else    
+    else
         alert("Sorry, " + fileName + " is invalid");
-    
+
 }
 //uploading user file using URL
-document.getElementById("imageFile").onchange = function(){previewFile()};
+document.getElementById("imageFile").onchange = function () { previewFile() };
 function previewFile() {
     // const preview = document.querySelector('img');
     const file = document.querySelector('input[type=file]').files[0];
     const reader = new FileReader();
-  
+
     reader.addEventListener("load", function () {
-      // convert image file to base64 string
+        // convert image file to base64 string
         checkFile(reader.result);
     }, false);
-  
+
     if (file) {
-      reader.readAsDataURL(file);
+        reader.readAsDataURL(file);
     }
 }
 
 
 loader.load("threed_objects/scene_2.glb",
-function(gltf){
-    // scene.remove(gltf.scene);
-    object = gltf.scene;
-    scene.add(gltf.scene);
-}, undefined, function ( error ) {
+    function (gltf) {
+        // scene.remove(gltf.scene);
+        object = gltf.scene;
+        scene.add(gltf.scene);
+    }, undefined, function (error) {
 
-    console.error( error );
-} );
+        console.error(error);
+    });
 
 
 
@@ -120,14 +120,14 @@ window.addEventListener(
 
 const scene = new THREE.Scene();
 const ambientLight = new THREE.AmbientLight(0xcccccc, 0.4);
-scene.add( ambientLight)
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+scene.add(ambientLight)
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const my_canvas = document.getElementById("canvas")
 
-const renderer = new THREE.WebGLRenderer({alpha:true, canvas:my_canvas});
+const renderer = new THREE.WebGLRenderer({ alpha: true, canvas: my_canvas });
 
-const controls = new OrbitControls( camera, renderer.domElement );
+const controls = new OrbitControls(camera, renderer.domElement);
 
 
 // const renderer = new THREE.WebGLRenderer({alpha:true});
@@ -141,33 +141,31 @@ var output = document.getElementById("demo");
 output.innerHTML = slider.value; // Display the default slider value
 
 const raycaster = new THREE.Raycaster();
-const mouse = new THREE.Vector2( 1, 1 );
+const mouse = new THREE.Vector2(1, 1);
 
-document.addEventListener( 'dblclick', onMouseMove );
-function onMouseMove( event ) {
+document.addEventListener('dblclick', onMouseMove);
+function onMouseMove(event) {
 
     event.preventDefault();
 
-    mouse.x = ( event.clientX / window.innerWidth ) * 2 - 1;
-    mouse.y = - ( event.clientY / window.innerHeight ) * 2 + 1;
+    mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+    mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
     object.position.set(mouse.x, mouse.y, 0);
 }
 
 function animate() {
-    
-    requestAnimationFrame( animate);
+
+    requestAnimationFrame(animate);
 
     slider.step = 0.25
     var x = slider.value
-    slider.oninput = function() {
-    output.innerHTML = this.value;
-    object.scale.set(x,x,x);
-}
+    slider.oninput = function () {
+        output.innerHTML = this.value;
+        object.scale.set(x, x, x);
+    }
 
-var xSpeed = 0.00001;
-var ySpeed = 0.00001;
-
-
+    var xSpeed = 0.00001;
+    var ySpeed = 0.00001;
 
     document.addEventListener("keydown", onDocumentKeyDown, false);
     function onDocumentKeyDown(event) {
@@ -185,19 +183,19 @@ var ySpeed = 0.00001;
         }
         //i may need help changing this to finger movements
 
-   controls.update();
-raycaster.setFromCamera( mouse, camera );
-const intersection = raycaster.intersectObject( object );
+        controls.update();
+        raycaster.setFromCamera(mouse, camera);
+        const intersection = raycaster.intersectObject(object);
 
-				if ( intersection.length > 0 ) {
+        if (intersection.length > 0) {
 
-					// const instanceId = intersection[ 0 ].instanceId;
-                    console.log("Intersect at ...")
-                }
-    
-}
+            // const instanceId = intersection[ 0 ].instanceId;
+            console.log("Intersect at ...")
+        }
 
-renderer.render(scene, camera);
+    }
+
+    renderer.render(scene, camera);
 }
 
 animate();
@@ -206,31 +204,18 @@ animate();
 //code for index.html
 var video = document.querySelector("#video_element");
 navigator.getUserMedia = (
-  navigator.getUserMedia ||
-  navigator.webkitGetUserMedia ||
-  navigator.mozGetUserMedia ||
-  navigator.msGetUserMedia
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia ||
+    navigator.msGetUserMedia
 );
 
 if (navigator.mediaDevices.getUserMedia) {
-  navigator.mediaDevices.getUserMedia({video: true})
-  .then(function(stream) {
-    video.srcObject = stream;
-  })
-  .catch(function (error){
-    alert("Something went wrong \nReload page");
-  })
+    navigator.mediaDevices.getUserMedia({ video: true })
+        .then(function (stream) {
+            video.srcObject = stream;
+        })
+        .catch(function (error) {
+            alert("Something went wrong \nReload page");
+        })
 }
-
-// function stopWebCam() {
-//     if (video) {
-//         video.pause();
-//         video.src = '';
-//         video.load();
-//     }
-
-//     if (cameraStream && cameraStream.stop) {
-//         cameraStream.stop();
-//     }
-//     stream = null;
-// }
